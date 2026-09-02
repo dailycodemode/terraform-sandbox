63c01e3102d546fdfbee8fa2d90b434752866cf2
dailycodemode <steedmanman@gmail.com>
2026-09-02 12:19:50 +0100

    tag03

--- vpc.tf
+++ vpc.tf
@@ -4,12 +4,12 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "basic-vpc2"
+    Name = "basic-vpc1"
   }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block           = "10.30.0.0/16"
+resource "aws_vpc" "vpc2" {
+  cidr_block           = "10.40.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
